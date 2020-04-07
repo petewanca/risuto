694339bfc1f03a6e933641a825926f12d97532e7
Pete Wanca <pwanca@xsolis.com>
2020-04-07 10:26:21 -0500

    adds conditional nav bar display based on auth

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,18 +1,60 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../context/Contexts/UserContext';
 
 export const Nav = () => {
-    return (
-        <ul>
-            <li>
-                <Link to='/'>Login</Link>
-            </li>
-            <li>
-                <Link to='/register'>Register</Link>
-            </li>
-            <li>
-                <Link to='/dashboard'>Dashboard</Link>
-            </li>
-        </ul>
-    );
+    const { user } = useContext(UserContext);
+    const auth = user.loggedIn;
+    let content;
+
+    auth
+        ? (content = (
+              <ul>
+                  <li>
+                      <Link to='/'>Logo</Link>
+                  </li>
+                  <li>
+                      <Link to='/'>Profile</Link>
+                  </li>
+                  <li>
+                      <Link to='/'>My Lists</Link>
+                  </li>
+                  <li>
+                      <Link to='/'>Forum</Link>
+                  </li>
+                  <li>
+                      <Link to='/'>Contact</Link>
+                  </li>
+                  <li>
+                      <Link to='/'>About</Link>
+                  </li>
+                  <li>
+                      <Link to='/logout'>Logout</Link>
+                  </li>
+              </ul>
+          ))
+        : (content = (
+              <ul>
+                  <li>
+                      <Link to='/'>Logo</Link>
+                  </li>
+                  <li>
+                      <Link to='/'>Forum</Link>
+                  </li>
+                  <li>
+                      <Link to='/'>Contact</Link>
+                  </li>
+                  <li>
+                      <Link to='/login'>About</Link>
+                  </li>
+                  <li>
+                      <Link to='/'>Login</Link>
+                  </li>
+                  <li>
+                      <Link to='/register'>Register</Link>
+                  </li>
+              </ul>
+          ));
+
+    return <div>{content}</div>;
 };
