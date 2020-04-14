72131543e350de349fdddd373497bfd467271e94
Pete Wanca <pwanca@xsolis.com>
2020-04-14 15:04:05 -0500

    adds fix to private route component continous server calls

--- client/src/utils/PrivateRoute.js
+++ client/src/utils/PrivateRoute.js
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useLayoutEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { Validate } from './Passport';
+import { Validate } from './Validate';
 import { Login } from '../pages/Login';
 import { UserContext } from '../context/Contexts/UserContext';
 
@@ -20,7 +20,15 @@ export const PrivateRoute = ({ path, component }) => {
         }
     };
 
-    sendValidation();
+    // from React's documentation...
+    // The signature is identical to useEffect, but it fires synchronously
+    // after all DOM mutations. Use this to read layout from the DOM and
+    // synchronously re-render. Updates scheduled inside useLayoutEffect
+    // will be flushed synchronously, before the browser has a chance to paint.
+    // Prefer the standard useEffect when possible to avoid blocking visual updates.
+    useLayoutEffect(() => {
+        sendValidation();
+    }, [auth]);
 
     return auth ? (
         <Route exact path={path} component={component} />
